4820c2acab44e537d950f8fe2e3adb9805e4aca3
sachin <sachin.singh@manektech.com>
2023-03-16 23:24:18 +0530

    twilio account

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express();
 
 
-const PORT = process.env.PORT || 3000;
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+
+const PORT = process.env.PORT || 3000;44
+
+app.post('/send', async (req, res) => {
+    const code = Math.floor(1000 + Math.random() * 9000);
+   try {
+     const data = await client.messages
+     .create({
+        body: ` hi rahul your code is ${code}`,
+        from: '+15076232990',
+        to: '+917041200380'
+      })
+
+
+      res.status(200).send(data.sid)
+
+   } catch (error) {
+    console.log(error.message)
+   }
+})
+
 
 
 app.listen(PORT, () => {
